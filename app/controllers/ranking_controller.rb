e09f81f715b516e7c0e17ff28514a739a62c0f72
Kiichi Koyasu <kiirono1@sde.cs.titech.ac.jp>
2015-03-29 22:12:34 +0900

    implement create method, that is post point and add record

--- app/controllers/ranking_controller.rb
+++ app/controllers/ranking_controller.rb
@@ -20,7 +20,12 @@ class RankingController < ApplicationController
   end
 
   def create
-    
+    @point = Point1.new(params.require(:ranking).permit(:name, :point))
+    if @point.save
+      render :json => @point
+    else
+      render :nothing => true, :status => 500
+    end
   end
 
   def update
